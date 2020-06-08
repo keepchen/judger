a191fab8d8ba5667dc5beb984efe84f5d5aa9980
keepchen <717502873@qq.com>
2020-06-08 14:08:08 +0800

    demo update

--- main.c
+++ main.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    char input[1000];
+    scanf("%s", input);
+    std::cout << input << std::endl;
+    std::cout << argc << std::endl;
+    std::cout << argv[0] << std::endl;
     std::cout << "hello world!" << std::endl;
     sleep(2);
     return 0;
--- run_test.sh
+++ run_test.sh
@@ -1,12 +1,15 @@
 #!/bin/env sh
 
 g++ main.c -o a.out && \
-        echo "g++ build success :)\nrunning test program...\n"
+        echo "g++ build success :)\nrunning test program...\n" && \
         ./shared-lib/output/libjudger.so \
         --exe_path="a.out" \
         --max_cpu_time=10000 \
         --max_real_time=10000 \
         --log_path="judger.log" \
+        --input_path="1.in" \
         --output_path="runtime.out" \
         --max_output_size=10000 \
-        --max_process_number=1024
+        --max_process_number=1024 \
+        --uid=0 \
+        --gid=0
